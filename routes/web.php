54dfde8e20f27b7e9639efc290f46462257a929a
alfianlw <alfianlistiyowibowo@gmail.com>
2022-05-20 17:00:43 +0700

    template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -9,10 +10,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/product', [ProductController::class, 'index'])->name('listbuku');
-Route::get('/product/buat', [ProductController::class, 'create'])->name('buatbuku');
-Route::post('/product/buat', [ProductController::class, 'simpan'])->name('simpanbuku');
+Route::prefix('admin')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('product', [ProductController::class, 'index'])->name('listbuku');
+    Route::get('product/buat', [ProductController::class, 'create'])->name('buatbuku');
+    Route::post('product/buat', [ProductController::class, 'simpan'])->name('simpanbuku');
+});
 
 // cart
 Route::get('/cart', [CartController::class, 'index'])->name('lihatcart');
@@ -22,4 +25,3 @@ Route::get('/cart/{id}/hapus', [CartController::class, 'destroy'])->name('hapusc
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
